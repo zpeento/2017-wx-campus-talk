08456f3ecb56da7aa4ffcaa58ab73954be71c48b
iceiceice <509887158@qq.com>
2017-03-16 19:32:59 +0800

    [update]添加下拉刷新

--- app/app.js
+++ app/app.js
@@ -1,2 +1,4 @@
 App({
+	onLaunch : function(){
+	}
 })
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -40,6 +40,22 @@ Page({
 			time:"16:00",
 			university:"华南理工大学",
 			location:"五山校区就业指导中心一号报告厅"
+		},{
+			company:"中国能源建设集团华南电力试验研究院有限公司",
+			date:"2017-03-15",
+			weekDay:"星期三",
+			today:"今天",
+			time:"16:00",
+			university:"华南理工大学",
+			location:"五山校区就业指导中心一号报告厅"
 		}]
+	},
+	onPullDownRefresh:function(){
+		//do something
+		console.log('haha');
+		wx.showNavigationBarLoading() //在标题栏中显示加载
+		wx.stopPullDownRefresh()
+		// wx.request({});
+		wx.hideNavigationBarLoading()
 	}
 })
